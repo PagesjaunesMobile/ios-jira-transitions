5f214737a661fd56c374478fba17bc503d6e0a07
AmineArous <aminearous@live.fr>
2021-01-19 22:29:11 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -5,5 +5,5 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 npm install --prefix $THIS_SCRIPT_DIR xhr2 --save 
 npm install --prefix $THIS_SCRIPT_DIR base-64 --save 
 
-
-$THIS_SCRIPT_DIR/transitions.js ${CHANGELOG} "${TRANSITION_TO}" "${JIRA_AUTH_USER}" "${JIRA_AUTH_PASSWORD}"
+echo $CHANGELOG
+$THIS_SCRIPT_DIR/transitions.js "${CHANGELOG}" "${TRANSITION_TO}" "${JIRA_AUTH_USER}" "${JIRA_AUTH_PASSWORD}"
